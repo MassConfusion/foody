074b88c3fc56d3c6b48e46e54d7edd249ca897ac
Kitti Gelle <gkitti92@gmail.com>
2021-10-08 17:05:30 +0200

    Exnted Pantry with Add food logic

--- lib/pantry.dart
+++ lib/pantry.dart
@@ -9,17 +9,72 @@ class Pantry extends StatefulWidget {
 }
 
 class PantryState extends State<Pantry> {
+  // TODO: create food db entity
+  final foodList = <String>[];
+  final TextEditingController textFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(pantryTitle)),
       body: ListView(
         restorationId: pantryRestorationId,
+        children: getFoodItems(),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => {},
+        onPressed: () => displayDialog(context),
+        tooltip: floatingButtonTooltip,
       ),
     );
+  }
+
+  void addFoodItem(String food) {
+    setState(() {
+      foodList.add(food);
+    });
+    textFieldController.clear();
+  }
+
+  Widget buildFoodItem(String food) {
+    return ListTile(title: Text(food));
+  }
+
+  List<Widget> getFoodItems() {
+    final foodWidgets = <Widget>[];
+    for (String food in foodList) {
+      foodWidgets.add(buildFoodItem(food));
+    }
+    return foodWidgets;
+  }
+
+  Future<AlertDialog?> displayDialog(BuildContext context) async {
+    return showDialog<AlertDialog>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text(dialogTitle),
+            content: TextField(
+              controller: textFieldController,
+              decoration: const InputDecoration(hintText: dialogHint),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  addFoodItem(textFieldController.text);
+                },
+                child: const Text(dialogActionAdd),
+                autofocus: true,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text(dialogActionCancel),
+              ),
+            ],
+          );
+        });
   }
 }
